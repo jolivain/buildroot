3be645d4b998e6d5849855c8e5ddc234aec982fb
Julien Olivain <ju.o@free.fr>
2022-07-03 17:43:21 +0200

    package/octave: add support for cross compile tools
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/octave/octave.mk
+++ package/octave/octave.mk
@@ -10,8 +10,9 @@ OCTAVE_SOURCE = octave-$(OCTAVE_VERSION).tar.lz
 OCTAVE_LICENSE = GPL-3.0+
 OCTAVE_LICENSE_FILES = COPYING
 OCTAVE_AUTORECONF = YES
+OCTAVE_INSTALL_STAGING = YES
 
-OCTAVE_CONF_OPTS = --disable-java
+OCTAVE_CONF_OPTS = --disable-java --enable-cross-tools
 
 OCTAVE_DEPENDENCIES = \
 	host-gperf \
@@ -24,5 +25,13 @@ OCTAVE_DEPENDENCIES += readline
 else
 OCTAVE_CONF_OPTS += --disable-readline
 endif
+
+define OCTAVE_INSTALL_CROSSTOOLS
+	cp \
+		$(@D)/src/$(GNU_TARGET_NAME)-mkoctfile \
+		$(@D)/src/$(GNU_TARGET_NAME)-octave-config \
+		$(HOST_DIR)/usr/bin/
+endef
+OCTAVE_POST_INSTALL_STAGING_HOOKS += OCTAVE_INSTALL_CROSSTOOLS
 
 $(eval $(autotools-package))
